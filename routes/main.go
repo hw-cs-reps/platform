971743d5b74df27c7c4e63507016c1577fc2b419
Humaid AlQassimi <git@humaidq.ae>
2020-07-20 11:26:37 +0400

    Load configuration on homepage.

--- routes/main.go
+++ routes/main.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	"github.com/hw-cs-reps/platform/config"
+
 	"github.com/go-macaron/session"
 	macaron "gopkg.in/macaron.v1"
 )
 
 // HomepageHandler response for the home page.
 func HomepageHandler(ctx *macaron.Context, sess session.Store, f *session.Flash) {
+	ctx.Data["Config"] = config.Config.InstanceConfig
 	ctx.Data["IsHome"] = 1
 	ctx.HTML(200, "index")
 }
