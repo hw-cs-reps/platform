6e7b44da78f87075c23078f5600b6d3abadc2ea5
Humaid AlQassimi <git@humaidq.ae>
2020-07-25 22:01:12 +0400

    Filter subject line in mailer.

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -3,7 +3,6 @@ package mailer
 import (
 	"github.com/hw-cs-reps/platform/config"
 
-	"fmt"
 	"net/smtp"
 	"strings"
 	"time"
@@ -22,16 +21,19 @@ func formatTo(to []string) string {
 	return str.String()
 }
 
+// newlineFilter replaces newlines with a space.
+var newlineFilter = strings.NewReplacer("\r\n", " ",
+	"\r", " ",
+	"\n", " ")
+
 // Email sends an email to a specific email address.
 func Email(to []string, title string, message string) (err error) {
 	from := config.Config.EmailAddress
 	full := "From: <" + from + ">\n" +
 		"To: " + formatTo(to) + "\n" +
 		"Date: " + time.Now().Format(time.RFC1123Z) + "\n" +
-		"Subject: " + title + "\n\n" +
+		"Subject: " + newlineFilter.Replace(title) + "\n\n" +
 		message
-
-	fmt.Println(full)
 
 	err = smtp.SendMail(config.Config.EmailSMTPServer,
 		smtp.PlainAuth("", from, config.Config.EmailPassword, strings.Split(config.Config.EmailSMTPServer, ":")[0]),
