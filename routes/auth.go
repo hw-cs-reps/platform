2e459bd5db7b1560d03f8d686b8313b83c4a4d7b
Humaid AlQassimi <git@humaidq.ae>
2020-07-27 19:05:44 +0400

    Split routes into separate files.

---
+++ routes/auth.go
@@ -0,0 +1,122 @@
+package routes
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/go-macaron/csrf"
+	"github.com/go-macaron/session"
+	"github.com/hw-cs-reps/platform/config"
+	"github.com/hw-cs-reps/platform/mailer"
+	macaron "gopkg.in/macaron.v1"
+)
+
+// LoginHandler response for the login page.
+func LoginHandler(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF) {
+	ctx.Data["Title"] = config.Config.SiteName
+	ctx.Data["csrf_token"] = x.GetToken()
+	ctx.HTML(200, "login")
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randIntRange(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+// PostLoginHandler post response for the login page.
+func PostLoginHandler(ctx *macaron.Context, sess session.Store, f *session.Flash) {
+	if sess.Get("auth") == Verification {
+		ctx.Redirect("/verify")
+		return
+	} else if sess.Get("auth") == LoggedIn {
+		ctx.Redirect("/")
+		return
+	}
+	for _, c := range config.Config.InstanceConfig.ClassReps {
+		if c.Email == ctx.QueryTrim("email")+config.Config.UniEmailDomain {
+			// Generate code
+			code := fmt.Sprint(randIntRange(100000, 999999))
+			// Note: We assume email in config to be correct
+
+			if !config.Config.DevMode {
+				go mailer.EmailCode(c.Email, code)
+			}
+			sess.Set("auth", Verification)
+			sess.Set("code", code)
+			sess.Set("user", c.Email)
+			sess.Set("attempts", 0)
+
+			ctx.Redirect("/verify")
+			return
+		}
+	}
+
+	f.Error("You are not registered.")
+	ctx.Redirect("/login")
+}
+
+// VerifyHandler post response for the login page.
+func VerifyHandler(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF) {
+	if sess.Get("auth") == LoggedOut {
+		ctx.Redirect("/login")
+		return
+	} else if sess.Get("auth") == LoggedIn || sess.Get("auth") != Verification {
+		f.Info("You are already logged in!")
+		ctx.Redirect("/")
+		return
+	}
+	ctx.Data["csrf_token"] = x.GetToken()
+	ctx.Data["email"] = sess.Get("user")
+	ctx.Data["Title"] = "Verification"
+	ctx.HTML(200, "verify_login")
+}
+
+// PostVerifyHandler post response for the login page.
+func PostVerifyHandler(ctx *macaron.Context, sess session.Store, f *session.Flash) {
+	if sess.Get("auth") == LoggedOut {
+		ctx.Redirect("/login")
+		return
+	} else if sess.Get("auth") == LoggedIn {
+		ctx.Redirect("/")
+		return
+	}
+	sess.Set("attempts", sess.Get("attempts").(int)+1)
+	if sess.Get("attempts").(int) > 3 {
+		f.Error("You reached the maximum number of attempts. Please try again later.")
+		sess.Set("auth", LoggedOut)
+		ctx.Redirect("/")
+		return
+	}
+	if ctx.QueryTrim("code") != sess.Get("code") && !config.Config.DevMode {
+		f.Error("The code you entered is invalid, make sure you use the latest code sent to you.")
+		ctx.Redirect("/verify")
+		return
+	}
+
+	sess.Set("auth", LoggedIn)
+	sess.Set("isadmin", 1)
+	ctx.Redirect("/")
+}
+
+// CancelHandler post response for canceling verification.
+func CancelHandler(ctx *macaron.Context, sess session.Store) {
+	if sess.Get("auth") != Verification {
+		ctx.Redirect("/login")
+		return
+	}
+
+	sess.Set("auth", LoggedOut)
+	ctx.Redirect("/login")
+}
+
+// LogoutHandler response for the login page.
+func LogoutHandler(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF) {
+	sess.Set("auth", LoggedOut)
+	sess.Set("isadmin", 0)
+	sess.Set("user", "")
+	ctx.Redirect("/")
+}
---
+++ routes/complaints.go
@@ -0,0 +1,102 @@
+package routes
+
+import (
+	"github.com/go-macaron/csrf"
+	"github.com/go-macaron/session"
+	"github.com/hw-cs-reps/platform/config"
+	"github.com/hw-cs-reps/platform/mailer"
+	macaron "gopkg.in/macaron.v1"
+)
+
+// ComplaintsHandler response for the complaints page.
+func ComplaintsHandler(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF) {
+	ctx.Data["Title"] = "Complaints"
+	ctx.Data["IsComplaints"] = 1
+	ctx.Data["Courses"] = config.Config.InstanceConfig.Courses
+	ctx.Data["csrf_token"] = x.GetToken()
+	ctx.HTML(200, "complaints")
+}
+
+func getClassRepsByCourseCode(code string) (recipients []*config.ClassRepresentative) {
+	var degrees []string
+	for _, c := range config.Config.InstanceConfig.Courses {
+		if c.Code == code {
+			degrees = c.DegreeCode
+			break
+		}
+	}
+
+	for i, c := range config.Config.InstanceConfig.ClassReps {
+		for _, d := range degrees {
+			if c.DegreeCode == d {
+				recipients = append(recipients, &config.Config.InstanceConfig.ClassReps[i])
+				break
+			}
+		}
+	}
+	return
+}
+
+// PostComplaintsHandler response for the complaints page.
+func PostComplaintsHandler(ctx *macaron.Context, sess session.Store, f *session.Flash, x csrf.CSRF) {
+	ctx.Data["IsComplaints"] = 1
+	if ctx.QueryTrim("confirm") == "1" { // confirm sending
+		var sender string
+		if ctx.QueryTrim("email") == "" {
+			sender = "anonymous"
+		} else {
+			sender = ctx.QueryTrim("email")
+		}
+		// TODO send to respective class reps
+
+		var recipients []string
+		if ctx.QueryTrim("category") == "general" {
+			for _, c := range config.Config.InstanceConfig.ClassReps {
+				recipients = append(recipients, c.Email)
+			}
+		} else {
+			crs := getClassRepsByCourseCode(ctx.QueryTrim("category"))
+			for _, c := range crs {
+				recipients = append(recipients, c.Email)
+			}
+		}
+
+		mailer.Email(recipients, "Complaint submission", `A complaint submission
+From: `+sender+`
+Category: `+ctx.QueryTrim("category")+`
+Subject: `+ctx.QueryTrim("subject")+`
+Message:
+`+ctx.QueryTrim("message"))
+
+		f.Success("Your complaint was sent!")
+		ctx.Redirect("/complaints")
+		return
+	}
+
+	ctx.Data["Category"] = ctx.QueryTrim("category")
+	ctx.Data["Subject"] = ctx.QueryTrim("subject")
+	ctx.Data["Message"] = ctx.QueryTrim("message")
+	ctx.Data["Email"] = ctx.QueryTrim("Email")
+	ctx.Data["csrf_token"] = x.GetToken()
+
+	var recipients []string
+	if ctx.QueryTrim("category") == "general" {
+		for _, c := range config.Config.InstanceConfig.ClassReps {
+			recipients = append(recipients, c.Name)
+		}
+	} else {
+		crs := getClassRepsByCourseCode(ctx.QueryTrim("category"))
+		if len(crs) == 0 {
+			f.Error("Sorry, no class representatives are available for the selected course/category.")
+			ctx.Redirect("/complaints")
+			return
+		}
+
+		for _, c := range crs {
+			recipients = append(recipients, c.Name)
+		}
+	}
+
+	ctx.Data["Recipients"] = recipients
+	ctx.HTML(200, "complaints-confirm")
+}
